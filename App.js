972c570e59a3eae36ff79b8ebbb5393cf4d59032
deepakOptimizeApp <deepak@optimizeapp.com>
2023-05-22 18:55:50 +0530

    add expo image picker

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+import * as ImagePicker from 'expo-image-picker'
 
 export default function App() {
+
+  const openGallery=async()=>{
+    console.log("openGallery")
+    try {
+      const result=await ImagePicker.launchImageLibraryAsync({
+        mediaTypes:ImagePicker.MediaTypeOptions.All,
+        exif:false,
+        quality: 1,
+        base64:false
+      })
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+ 
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <TouchableOpacity onPress={()=>openGallery()}>
+      <Text>Select image</Text>
+    </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
